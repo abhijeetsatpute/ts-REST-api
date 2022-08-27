389936c6a1f1f5d68dee2dc22f4bcf8cec34b372
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-27 21:55:40 +0530

    508. Added routes to update and delete todos

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import { Todo } from '../models/todo';
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -17,6 +17,22 @@ router.post('/todo', (req, res, next) => {
     }
 
     todos.push(newTodo);
+    res.status(201).json({ message: "Added Todo", todo: newTodo, todos: todos });
 })
+
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex(todo => todo.id === tid);
+    if(todoIndex >= 0) {
+        todos[todoIndex].text = req.body.text;
+        return res.status(200).json({ message: "Updated", todos: todos });
+    }
+    res.status(404).json({ message: "Could not find todo for this id."})
+});
+
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    res.status(2000).json({ message: "Deleted todo", todos: todos });
+});
 
 export default router;
