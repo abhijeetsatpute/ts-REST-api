d8e470a257f5af62781dd5363115f6bceab6400c
Abhijeet Satpute <abhi111sat@gmail.com>
2022-08-27 22:09:39 +0530

    510. Using Type Casting: Creating aliases for type checks

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 
 import { Todo } from '../models/todo';
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 let todos: Todo[] = [];
 
 const router = Router();
@@ -11,9 +14,10 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody;
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     }
 
     todos.push(newTodo);
@@ -21,18 +25,21 @@ router.post('/todo', (req, res, next) => {
 })
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const params = req.params as RequestParams;
+    const tid = params.todoId;
+    const body = req.body as RequestBody;
     const todoIndex = todos.findIndex(todo => todo.id === tid);
     if(todoIndex >= 0) {
-        todos[todoIndex].text = req.body.text;
+        todos[todoIndex].text = body.text;
         return res.status(200).json({ message: "Updated", todos: todos });
     }
     res.status(404).json({ message: "Could not find todo for this id."})
 });
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
-    res.status(2000).json({ message: "Deleted todo", todos: todos });
+    const params = req.params as RequestParams;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId);
+    res.status(200).json({ message: "Deleted todo", todos: todos });
 });
 
 export default router;
